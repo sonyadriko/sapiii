de3d323cb620996f2e90d9d98a59d7cee2da1ea4
sonyadriko <sonyadiadriko@gmail.com>
2023-02-01 20:34:05 +0700

    layout detail sapi,kambing dan grafik sapi

---
+++ app/src/main/java/com/example/sapiii/DetailKambingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sapiii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailKambingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_kambing)
+    }
+}
