e1e09845be3449e73cc56941c82a1de86b05b7ee
sonyadriko <sonyadiadriko@gmail.com>
2023-02-01 22:05:09 +0700

    use fragment

--- app/src/main/java/com/example/sapiii/HomeActivity.kt
+++ app/src/main/java/com/example/sapiii/HomeActivity.kt
@@ -2,10 +2,34 @@ package com.example.sapiii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sapiii.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(homeFragment: HomeFragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, homeFragment)
+        fragmentTransaction.commit()
     }
 }
