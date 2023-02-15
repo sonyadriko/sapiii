d76f44d3fd20a9f6cccbaaaf58637f351b327127
sonyadriko <sonyadiadriko@gmail.com>
2023-02-16 01:29:44 +0700

    update icon tapi recycler view sek eror

--- app/src/main/java/com/example/sapiii/feature/ternakku/ListSapiFragment.kt
+++ app/src/main/java/com/example/sapiii/feature/ternakku/ListSapiFragment.kt
@@ -21,13 +21,18 @@ import com.google.firebase.database.DatabaseReference
 
 class ListSapiFragment : Fragment() {
 
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private val ARG_PARAM1 = "param1"
+    private val ARG_PARAM2 = "param2"
+
 //    private lateinit var dbref : DatabaseReference
 //    private lateinit var sapiRecyclerView : RecyclerView
 //    private lateinit var sapiArrayList : ArrayList<Sapi>
 
     private lateinit var viewModel : SapiViewModel
-    private lateinit var userRecyclerView: RecyclerView
-    lateinit var adapter: SapiAdapter
+    private lateinit var userRecyclerView : RecyclerView
+    lateinit var adapter : SapiAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,31 +58,20 @@ class ListSapiFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            com.example.sapiii.ListSapiFragment().apply {
+            ListSapiFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
 
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private const val ARG_PARAM1 = "param1"
-        private const val ARG_PARAM2 = "param2"
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         userRecyclerView = view.findViewById(R.id.sapi_list)
         userRecyclerView.layoutManager = LinearLayoutManager(context)
         userRecyclerView.setHasFixedSize(true)
