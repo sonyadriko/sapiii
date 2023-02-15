1ad0dfb8495cd170ecbaf5bb63a7ed6b04fbfe21
sonyadriko <sonyadiadriko@gmail.com>
2023-02-15 19:29:38 +0700

    heh iki eror list sapi frgment

---
+++ app/src/main/java/com/example/sapiii/SapiAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.sapiii
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sapiii.domain.Sapi
+import org.w3c.dom.Text
+
+class SapiAdapter : RecyclerView.Adapter<SapiAdapter.MyViewHolder>() {
+
+    private val sapiList = ArrayList<Sapi>()
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_view_sapi, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+        val currentItem = sapiList[position]
+
+        holder.namaSapi.text = currentItem.namaSapi
+        holder.genderSapi.text = currentItem.jkSapi
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return sapiList.size
+        TODO("Not yet implemented")
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val namaSapi : TextView = itemView.findViewById(R.id.text_cow_name)
+        val genderSapi : TextView = itemView.findViewById(R.id.text_cow_gender)
+
+    }
+
+
+}
+
--- app/src/main/java/com/example/sapiii/feature/ternakku/ListSapiFragment.kt
+++ app/src/main/java/com/example/sapiii/feature/ternakku/ListSapiFragment.kt
@@ -1,19 +1,44 @@
 package com.example.sapiii.feature.ternakku
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sapiii.R
+import com.example.sapiii.TambahDataSapiActivity
+import com.example.sapiii.domain.Sapi
+import com.google.firebase.database.DatabaseReference
 
 class ListSapiFragment : Fragment() {
+
+    private lateinit var dbref : DatabaseReference
+    private lateinit var sapiRecyclerView : RecyclerView
+    private lateinit var sapiArrayList : ArrayList<Sapi>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val v = inflater.inflate(R.layout.fragment_list_sapi, container, false)
+        val tmbhsapi = v.findViewById<Button>(R.id.btn_tambah_data_sapi)
+
+        tmbhsapi.setOnClickListener{
+            startActivity(Intent(activity, TambahDataSapiActivity::class.java))
+        }
+
+        sapiRecyclerView = v.findViewById(R.id.sapi_list)
+        sapiRecyclerView.layoutManager = LinearLayoutManager(this)
+        sapiRecyclerView.setHasFixedSize(true)
+        
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_sapi, container, false)
+        return v
     }
 }
