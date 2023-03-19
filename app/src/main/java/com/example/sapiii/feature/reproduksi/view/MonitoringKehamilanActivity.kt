88d5e8a25018838a1afa9ae2a4dc033b99c8965b
sonyadriko <sonyadiadriko@gmail.com>
2023-03-20 01:38:38 +0700

    fix monitoring kehamilan

--- app/src/main/java/com/example/sapiii/feature/reproduksi/view/MonitoringKehamilanActivity.kt
+++ app/src/main/java/com/example/sapiii/feature/reproduksi/view/MonitoringKehamilanActivity.kt
@@ -5,7 +5,13 @@ import android.widget.ArrayAdapter
 import com.example.sapiii.base.BaseActivity
 import com.example.sapiii.constanst.Constant
 import com.example.sapiii.databinding.ActivityMonitoringKehamilanBinding
+import com.example.sapiii.domain.MonitoringKehamilan
+import com.example.sapiii.domain.MonitoringPejantan
 import com.example.sapiii.repository.KambingRepository
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MonitoringKehamilanActivity : BaseActivity() {
 
@@ -17,6 +23,34 @@ class MonitoringKehamilanActivity : BaseActivity() {
         setContentView(binding.root)
 
         fetchNamaKodeData()
+
+        val database = Firebase.database
+        val myRef = database.getReference("Monitoring_Kehamilan")
+
+        binding.btnSaveMk.setOnClickListener{
+            val nama = binding.spinnerkp.selectedItem.toString()
+            val dateString = binding.etDateKehamilan.text.toString()
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+            val tanggal: Date = dateFormat.parse(dateString)
+//            val dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+//            val tanggal: LocalDate = LocalDate.parse(dateString, dateFormat)
+            val kodeKandang = binding.etKodeKandangMk.text.toString()
+            val calendar = Calendar.getInstance()
+            calendar.time = tanggal
+            calendar.add(Calendar.MONTH, 9)
+            val tanggal2 = calendar.time // Mengonversi ke objek Date
+
+
+            val monitoringKehamilan = MonitoringKehamilan(nama, kodeKandang, tanggal, tanggal2)
+            myRef.child(monitoringKehamilan.nama).setValue(monitoringKehamilan)
+                .addOnSuccessListener {
+                    showToast("Data berhasil disimpan")
+                    finish()
+                }
+                .addOnFailureListener {
+                    showToast("Data gagal disimpan")
+                }
+        }
     }
 
     private fun fetchNamaKodeData() = with(binding) {
